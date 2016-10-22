d97185e12f470aaa01c14210aff738ea6ca1871b
Mayank Makwana <mayankmmmx@gmail.com>
2016-10-21 21:12:31 -0400

    initial commit

---
+++ api/application.py
@@ -0,0 +1,18 @@
+# hack for harambe
+from flask import Flask, Response, request, jsonify
+application = Flask(__name__)
+
+
+@application.route('/')
+@application.route('/index')
+def index():
+    return "Welcome to Reminder's RESTful API!"
+'''
+@application.route('/harambe/diagnosis', methods=['POST'])
+def route_first():
+    if request.headers['Content-Type'] == 'application/json':
+        return jsonify(Routes.diagnosis.respond(request.get_json()))
+'''
+
+if __name__ == "__main__":
+    application.run(debug=True)
