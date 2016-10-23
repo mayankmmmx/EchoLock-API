420086d94b87b3a8908979c500fc3188cd27d364
Mayank Makwana <mayankmmmx@gmail.com>
2016-10-23 04:34:33 -0400

    app

--- api/application.py
+++ api/application.py
@@ -8,20 +8,24 @@ application = Flask(__name__)
 
 @application.route('/')
 @application.route('/index')
+@crossdomain(origin='*')
 def index():
     return "Welcome to EchoLock's RESTful API!"
 
 @application.route('/harambe/register', methods=['POST'])
+@crossdomain(origin='*')
 def register():
     if request.headers['Content-Type'] == 'application/json':
         return jsonify(Routes.register.respond(request.get_json()))
 
 @application.route('/harambe/login', methods=['POST'])
+@crossdomain(origin='*')
 def login():
     if request.headers['Content-Type'] == 'application/json':
         return jsonify(Routes.login.respond(request.get_json()))
 
 @application.route('/harambe/add_site', methods=['POST'])
+@crossdomain(origin='*')
 def add_site():
     if request.headers['Content-Type'] == 'application/json':
         return jsonify(Routes.add_site.respond(request.get_json()))
