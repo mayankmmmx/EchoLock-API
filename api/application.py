524a36b2bfa6d40fe69e5bb64461b7e8a43727d9
Mayank Makwana <mayankmmmx@gmail.com>
2016-10-23 01:15:43 -0400

    added socketio

--- api/application.py
+++ api/application.py
@@ -1,7 +1,8 @@
 # hack for harambe
 from flask import Flask, Response, request, jsonify
+from flask_socketio import SocketIO, send, emit
 application = Flask(__name__)
-
+socketio = SocketIO(application)
 
 @application.route('/')
 @application.route('/index')
@@ -14,5 +15,13 @@ def route_first():
         return jsonify(Routes.diagnosis.respond(request.get_json()))
 '''
 
+
+@socketio.on('value changed')
+def value_changed(message):
+    print(message)
+    emit('update value', "temp", broadcast=True)
+
+
 if __name__ == "__main__":
     application.run(debug=True)
+    socketio.run(application)
